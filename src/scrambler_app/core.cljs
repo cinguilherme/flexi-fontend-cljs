a2238a9b1e499334f83924683e1203064102a7d5
Cintra, Guilherme <cinguilherme@gmail.com>
2021-05-25 20:07:58 -0300

    feat: 🎸 add second version of form working with state

--- src/scrambler_app/core.cljs
+++ src/scrambler_app/core.cljs
@@ -6,8 +6,29 @@
 ;; Data
 (def scrambleData (r/atom {:sourc "abc" :targ "acd" :scramble? false}))
 
+;; Const
+(def baseUrl "http://localhost:8080/?str1=%22 %22&str2=%22 %22")
+
 ;; -------------------------
 ;; Views
+
+(defn scram-form [] 
+  (let [newInp (r/atom "")]
+    (fn [] 
+      [:form {:on-submit (fn [e] 
+        (.preventDefault e)
+        ()
+      )}
+        [:input {:type "text" 
+                  :value @newInp 
+                  :placeholder "first str here"
+                  :on-change (fn [e] 
+                            (reset! newInp (.-value (.-target e)))) }]
+        [:input {:type "text" :placeholder "second str here"}]
+        [:input {:type "button" :value "Request Scramble Check"}]
+        ]
+      )
+    ))
 
 (defn title-component []
   [:h2 {:class "col display-4"} "Welcome to Scrambler App"])
@@ -33,12 +54,17 @@
    [:div {:class "container"}
     [:div {:class "row"}
      [:div {:class "col-md-10" :style {:align "center" :padding "10px" :margin "10px"}}
+        
         [:br]
-        (title-component)
+        [title-component]
+        
         [:br]
-        (scramble-form)]
+        [scramble-form]
+        
         [:br]
-        (results-presenter @scrambleData)]]])
+        [scram-form]
+
+        (results-presenter @scrambleData)]]]])
 
 
 
