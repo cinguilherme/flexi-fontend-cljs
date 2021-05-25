38603481dc00cef3cb501b9d9f8a897527d2d609
Cintra Guilherme <cinguilherme@gmail.com>
2021-05-24 21:54:41 -0300

    feat: 🎸 initial commit, not running on m1

---
+++ src/scrambler_app/core.cljs
@@ -0,0 +1,19 @@
+(ns scrambler-app.core
+    (:require
+      [reagent.core :as r]
+      [reagent.dom :as d]))
+
+;; -------------------------
+;; Views
+
+(defn home-page []
+  [:div [:h2 "Welcome to Reagent"]])
+
+;; -------------------------
+;; Initialize app
+
+(defn mount-root []
+  (d/render [home-page] (.getElementById js/document "app")))
+
+(defn ^:export init! []
+  (mount-root))
